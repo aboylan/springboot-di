cde3cb8f1876799b98471e6542a33b2ea91c8b27
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-10 11:08:56 -0600

    Creando la clase Model y la clase Repository

---
+++ src/main/java/com/aboylan/springboot/di/app/springbootdi/repositories/ProductoRepository.java
@@ -0,0 +1,24 @@
+package com.aboylan.springboot.di.app.springbootdi.repositories;
+
+import java.util.Arrays;
+import java.util.List;
+
+import com.aboylan.springboot.di.app.springbootdi.models.Product;
+
+public class ProductoRepository {
+
+    private List<Product> data;
+
+    public ProductoRepository() {
+        this.data = Arrays.asList(
+                new Product(1L, "Memoria Corsair 32", 300L),
+                new Product(2L, "CPU Intel Core i9", 850L),
+                new Product(3L, "Teclado Rezer Mini 60%", 180L),
+                new Product(4L, "Motherboard Gigabyte", 409L));
+    }
+
+    public List<Product> findAll() {
+        return data;
+    }
+
+}
