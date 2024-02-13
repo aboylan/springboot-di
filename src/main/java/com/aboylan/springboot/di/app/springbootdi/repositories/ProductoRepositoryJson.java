f5a7dcf691f6f2bb7fcb8ba06b78bb198dd0d850
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-13 12:46:12 -0600

    Creando clase que lee los datos de un archivo json

---
+++ src/main/java/com/aboylan/springboot/di/app/springbootdi/repositories/ProductoRepositoryJson.java
@@ -0,0 +1,45 @@
+package com.aboylan.springboot.di.app.springbootdi.repositories;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.stereotype.Repository;
+
+import com.aboylan.springboot.di.app.springbootdi.models.Product;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Repository
+public class ProductoRepositoryJson implements ProductRepository {
+
+    private List<Product> list;
+
+    public ProductoRepositoryJson() {
+        Resource resource = new ClassPathResource("json/product.json");
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            list = Arrays.asList(objectMapper.readValue(resource.getFile(), Product[].class));
+        } catch (StreamReadException e) {
+            e.printStackTrace();
+        } catch (DatabindException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return null;
+    }
+
+    @Override
+    public Product findById(Long id) {
+        return null;
+    }
+
+}
