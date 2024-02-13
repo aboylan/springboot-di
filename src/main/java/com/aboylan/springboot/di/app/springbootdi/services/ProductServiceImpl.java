8d93e03abb52184c48e7b33ede0a3ce611e170cf
Alex Boylan <aboylan@users.noreply.github.com>
2024-02-13 12:30:45 -0600

    Revision tarea: leer desde un archivo de configuracion properties el valor tax

--- src/main/java/com/aboylan/springboot/di/app/springbootdi/services/ProductServiceImpl.java
+++ src/main/java/com/aboylan/springboot/di/app/springbootdi/services/ProductServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.aboylan.springboot.di.app.springbootdi.models.Product;
@@ -11,6 +12,9 @@ import com.aboylan.springboot.di.app.springbootdi.repositories.ProductRepository
 
 @Service
 public class ProductServiceImpl implements ProductService {
+
+    @Value("${config.tax.price}")
+    private Double tax;
 
     private ProductRepository repository;
 
@@ -21,15 +25,11 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public List<Product> findAll() {
         return repository.findAll().stream().map(p -> {
-            Double priceTax = p.getPrice() * 1.25d;
-            // Product newProd = new Product(p.getId(), p.getName(), priceTax.longValue());
-
+            System.out.println(tax);
+            Double priceTax = p.getPrice() * tax;
             Product newProd = (Product) p.clone();
             newProd.setPrice(priceTax.longValue());
             return newProd;
-
-            // p.setPrice(priceTax.longValue());
-            // return p;
         }).collect(Collectors.toList());
     }
 
